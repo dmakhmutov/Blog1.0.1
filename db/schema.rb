13c223aa6ef7d999eee502890f458dae6a7b9b6d
Vermuth <dmsunbox@gmail.com>
2012-11-15 11:06:02 +0400

    create migration to production commenting string 'About us' in application.html.erb change adapter in production   sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,23 @@ ActiveRecord::Schema.define(:version => 20121107090112) do
     t.string "name"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'bool' for column 'admin'
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.boolean  "admin"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
